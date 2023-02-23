f3e0b00408b593afe0f8b6fdeccaff6b77e75c3d
duong141001 <107864382+duong141001@users.noreply.github.com>
2023-02-23 16:35:33 +0700

    push home-frame

--- src/main/java/HomeFrame.java
+++ src/main/java/HomeFrame.java
@@ -1,5 +1,9 @@
 
-import java.awt.Dimension;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -8,7 +12,7 @@ import java.awt.Dimension;
 
 /**
  *
- * @author Tcom
+ * @author ACER
  */
 public class HomeFrame extends javax.swing.JFrame {
 
@@ -16,7 +20,8 @@ public class HomeFrame extends javax.swing.JFrame {
      * Creates new form HomeFrame
      */
     public HomeFrame() {
-        initComponents();
+         initComponents();
+           
     }
 
     /**
@@ -28,74 +33,182 @@ public class HomeFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        TitleHome = new javax.swing.JLabel();
+        CodeSaleButton = new javax.swing.JButton();
+        CustomerButton = new javax.swing.JButton();
+        OrderButton = new javax.swing.JButton();
+        AdminButton = new javax.swing.JButton();
+        BillButton = new javax.swing.JButton();
+        DrinkButton = new javax.swing.JButton();
+        LogoutButton = new javax.swing.JButton();
+
+        jLabel1.setText("jLabel1");
+
+        jLabel2.setText("jLabel2");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Jcafe88");
+
+        TitleHome.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        TitleHome.setForeground(new java.awt.Color(102, 0, 102));
+        TitleHome.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        TitleHome.setIcon(new javax.swing.ImageIcon("C:\\Users\\ACER\\Documents\\NetBeansProjects\\jcoffee\\src\\main\\java\\image\\icons8-coffee-beans-50.png")); // NOI18N
+        TitleHome.setText("WELCOME TO JCAFE88!!!");
+
+        CodeSaleButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\ACER\\Documents\\NetBeansProjects\\jcoffee\\src\\main\\java\\image\\sale resize.png")); // NOI18N
+        CodeSaleButton.setText("Code Sale");
+
+        CustomerButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\ACER\\Documents\\NetBeansProjects\\jcoffee\\src\\main\\java\\image\\customer resize.png")); // NOI18N
+        CustomerButton.setText("Customer");
+
+        OrderButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\ACER\\Documents\\NetBeansProjects\\jcoffee\\src\\main\\java\\image\\order resize.png")); // NOI18N
+        OrderButton.setText("Order");
+
+        AdminButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\ACER\\Documents\\NetBeansProjects\\jcoffee\\src\\main\\java\\image\\admin resize.png")); // NOI18N
+        AdminButton.setText("Admin");
+        AdminButton.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        //AdminButton.setBorder(new javax.swing.border.AbstractBorder(javax.swing.border.BevelBorder.RAISED));
+        AdminButton.setDefaultCapable(false);
+        AdminButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AdminButtonActionPerformed(evt);
+            }
+        });
+
+        BillButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\ACER\\Documents\\NetBeansProjects\\jcoffee\\src\\main\\java\\image\\bill resize.png")); // NOI18N
+        BillButton.setText("Bill");
+        BillButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BillButtonActionPerformed(evt);
+            }
+        });
+
+        DrinkButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\ACER\\Documents\\NetBeansProjects\\jcoffee\\src\\main\\java\\image\\drink resize.png")); // NOI18N
+        DrinkButton.setText("    Drink");
+
+        LogoutButton.setBackground(new java.awt.Color(255, 51, 51));
+        LogoutButton.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        LogoutButton.setForeground(new java.awt.Color(255, 255, 255));
+        LogoutButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\ACER\\Documents\\NetBeansProjects\\jcoffee\\src\\main\\java\\image\\icons8-logout-rounded-30.png")); // NOI18N
+        LogoutButton.setText("Logout");
+        LogoutButton.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        LogoutButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                LogoutButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 525, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(168, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(LogoutButton)
+                        .addGap(52, 52, 52))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(TitleHome, javax.swing.GroupLayout.PREFERRED_SIZE, 369, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(187, 187, 187))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(AdminButton, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(BillButton, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(DrinkButton, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(94, 94, 94)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(OrderButton, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(CustomerButton, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(CodeSaleButton, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGap(168, 168, 168))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 280, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(TitleHome, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(AdminButton, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(OrderButton, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(123, 123, 123)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(DrinkButton, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(CodeSaleButton, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(30, 30, 30)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(CustomerButton, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(BillButton, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 118, Short.MAX_VALUE)
+                        .addComponent(LogoutButton)
+                        .addGap(79, 79, 79))))
         );
 
+        getAccessibleContext().setAccessibleName("Welcome to Jcafe88\n");
+
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BillButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BillButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BillButtonActionPerformed
+
+    private void AdminButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AdminButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_AdminButtonActionPerformed
+
+    private void LogoutButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LogoutButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_LogoutButtonActionPerformed
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(HomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(HomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(HomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(HomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
+    public static void main(String args[]) throws IOException{
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-//                  HomeFrame h1 =  new HomeFrame();
-//                   jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//                 h1.setPreferredSize(new Dimension(1500, 0)); 
-//                h1.setVisible(true);
-               displayJFrame();
+        java.awt.EventQueue.invokeLater(() -> {
+            final String dir = System.getProperty("user.dir");
+            HomeFrame lf = new HomeFrame();
+            try {
+                lf.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File(dir + "\\src\\main\\java\\image\\background-home.jpg")))));
+            } catch (IOException e) {
             }
+            lf.pack();
+            lf.setLocationRelativeTo(null);
+//                lf.add(lf.jPanel1);
+            lf.add(lf.TitleHome);
+            lf.add(lf.AdminButton);
+            lf.add(lf.BillButton);
+            lf.add(lf.CodeSaleButton);
+            lf.add(lf.CustomerButton);
+            lf.add(lf.DrinkButton);
+            lf.add(lf.OrderButton);
+            lf.add(lf.LogoutButton);
+            lf.setVisible(true);
         });
     }
-    
-    static void displayJFrame()
-  {
-    // create our jframe as usual
-    HomeFrame jframe = new HomeFrame();
-    jframe.setDefaultCloseOperation(HomeFrame.EXIT_ON_CLOSE);
-
-    // set the jframe size and location, and make it visible
-    jframe.setPreferredSize(new Dimension(400, 300));
-    jframe.pack();
-    jframe.setLocationRelativeTo(null);
-    jframe.setVisible(true);
-  }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton AdminButton;
+    private javax.swing.JButton BillButton;
+    private javax.swing.JButton CodeSaleButton;
+    private javax.swing.JButton CustomerButton;
+    private javax.swing.JButton DrinkButton;
+    private javax.swing.JButton LogoutButton;
+    private javax.swing.JButton OrderButton;
+    private javax.swing.JLabel TitleHome;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
