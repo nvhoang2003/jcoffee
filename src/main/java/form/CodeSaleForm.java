60a75322127eb9e6b340d56bc6dd5edfae5b2e62
duong141001 <107864382+duong141001@users.noreply.github.com>
2023-02-26 15:24:08 +0700

    code sale valid

---
+++ src/main/java/form/CodeSaleForm.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package form;
+
+import com.mycompany.jcafe88.models.CodeSale;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ *
+ * @author ACER
+ */
+public class CodeSaleForm extends CodeSale {
+    public static Map<String, String> validated(String Name, String Sale_Off, String Code){
+        Map<String, String> validate_message = new HashMap<>();
+        if (Name.isBlank()) {
+            validate_message.put("Name", "Require a Name of Sale Event");
+        } else if (Name.length() > 4) {
+            validate_message.put("Name", "Name has been long than 4 character");        
+        }        
+        if (Sale_Off.isBlank()){
+            validate_message.put("Sale_Off", "Please put sale off number");
+        }
+        if (Code.isBlank()){
+            validate_message.put("Code", "Please input Code");
+        }       
+        return validate_message;
+    }    
+}
